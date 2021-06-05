38dd4fe52a677b2efe2a265e5f9e7231b7c7379f
le.trong <le.trong>
2021-06-05 19:16:47 -0400

    Played with typescript for aws lambda

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyEventV2, APIGatewayProxyResult } from 'aws-lambda';
 
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const queries = JSON.stringify(event.queryStringParameters);
+export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResult> => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    'Content-Type': 'application/json'
+  };
+
+  try {
+    switch (event.routeKey) {
+      case 'DELETE /portfolio/{id}':
+        body = { message: `DELETE portfolio ${event.pathParameters.id}` };
+        break;
+      case 'GET /portfolio/{id}':
+        body = { message: `GET portfolio ${event.pathParameters.id}` };
+        break;
+      case 'GET /portfolios':
+        body = body = { message: `GET all portfolios` };
+        break;
+      case 'PUT /portfolios':
+        let requestJSON = JSON.parse(event.body);
+        body = { message: `PUT portfolios`, request: requestJSON };
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
   return {
-    statusCode: 200,
-    body: `Queries: ${queries}`
+    statusCode,
+    body,
+    headers
   };
 };
