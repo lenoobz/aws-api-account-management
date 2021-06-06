5b1e2b5cb7bd952216081a127f12621fabc16742
le.trong <le.trong>
2021-06-06 18:19:56 -0400

    Updated PortfolioInfo

--- src/types/entities/portfolio.entity.ts
+++ src/types/entities/portfolio.entity.ts
@@ -2,9 +2,10 @@ export type PortfolioEntity = {
   id?: string;
   accountId: string;
   createdBy: string;
-  positions: PositionInfo;
+  positions: PositionInfo[];
 };
 
 export type PositionInfo = {
-  [ticker: string]: number;
+  ticker: string;
+  value: number;
 };
